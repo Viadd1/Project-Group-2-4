8fc88116eb4c9bdffdb4a8a5414d171f7cb7ee35
Viadd1 <david001207@gmail.com>
2026-02-27 13:33:25 +0100

    new file:   MCFS_plot.png 	new file:   Project.R 	new file:   Project2.csv 	new file:   clusterRules.R 	new file:   recalculateRules.R

---
+++ Project.R
@@ -0,0 +1,91 @@
+# Set working directory to where the dataset is located
+setwd("C:/Users/david/OneDrive - Uppsala universitet/Dokument/Uppsala Universitet/Biologi och bioteknik/Kunskapsbaserade system/Projekt")
+
+# Load appropriate packages
+library(rJava)
+library(rmcfs)
+library(R.ROSETTA)
+library(devtools)
+library(csVisuNet)
+
+# Read the data
+data <- read.csv("Project2.csv", sep="\t", colClasses = "character")
+
+# delete the id column
+data$id <- NULL
+
+# Calculate the threshold: Find columns where LESS THAN 50% of the data is "?"
+# (You can adjust the 0.50 to whatever threshold you prefer)
+cols_to_keep <- sapply(data, function(col) {
+  sum(col == "?") / length(col) < 0.50 
+})
+
+# Subset the data to keep only the good columns
+data_clean <- data[, cols_to_keep]
+
+data_clean$Host
+table(data_clean$Host)
+
+# MCFS
+mcfs_result <- mcfs(Host ~ ., 
+                    data = data_clean, 
+                    projections = 1500, 
+                    projectionSize = 0.1, 
+                    splits = 5,
+                    threadsNumber = 8,
+                    cutoffPermutations = 5
+                    )
+
+# Show selected features and plot convergence graph
+head(mcfs_result$RI)
+plot(mcfs_result, type="distances")
+
+# Plot relative importance graph and save features with RI>cutoff_value
+plot(mcfs_result, type = "ri", size = 50)
+mcfs_result2 <- mcfs_result$RI[1:mcfs_result$cutoff_value,]
+mcfs_result2
+
+# 5. Extract only the significant features
+top_features <- mcfs_result$RI[1:mcfs_result$cutoff_value, "attribute"]
+print(top_features)
+
+# Create a subset of the data containing ONLY the top features and the target 'Host'
+selected_data <- data_clean[, c(as.character(top_features), "Host")]
+
+selected_data
+
+# Train the rule-based model using 10-fold cross-validation
+rosetta_model <- rosetta(selected_data, 
+                         discrete = TRUE,
+                         reducer = "Johnson"
+                         #classifier = "StandardVoter",
+                         #roc = TRUE,
+                       #  cv = 10,
+                         #clroc = "Human"
+                       )
+
+# Retrieve rules and model performance (Accuracy, Sensitivity, Specificity)
+rules <- rosetta_model$main
+qual <- rosetta_model$quality
+print(rules)
+print(qual)
+
+# Show discretization levels (bins) used in the rules
+slevels <- rosetta_model$main$levels
+slevels
+
+# 1. Filter rules with a significant p-value
+sig_rules <- rosetta_model$main[rosetta_model$main$pValue < 0.05, ]
+sig_rules
+
+# If these functions give errors when running, download from github to wd and load them manually 
+#source("recalculateRules.R")
+#source("clusterRules.R")
+
+# Recalculate rules
+recalc_rules <- recalculateRules(selected_data, rosetta_model$main, discrete=TRUE)
+
+# NOTE: the clusterRules function takes training df and recalculated rules as arguments
+# Previously, the sig_rules table was used as an argument instead of training df, which caused issues in the figure
+clustered_rules <- clusterRules(selected_data, recalc_rules)
+
---
+++ recalculateRules.R
@@ -0,0 +1,153 @@
+recalculateRules <- function(dt, rules, discrete=FALSE, pAdjust=TRUE, pAdjustMethod="bonferroni"){
+  ##recalculate set of the rules
+  rl2 <- strsplit(as.character(rules$features), ",", fixed = T)
+  
+  if(discrete){
+    cnd2 <- strsplit(as.character(rules$levels), ",", fixed = T)
+  }else{
+    cnd2 <- strsplit(as.character(rules$cuts), ",", fixed = T)
+  }
+  
+  dec2 <- as.character(rules$decision)
+  objs <- rownames(dt)
+  feats <- colnames(dt)
+  cuts <- rules[,grep('cut', colnames(rules), value=TRUE)][,-1]
+  cuts <- as.data.frame(cuts)
+  ### functions ###
+  less2Vec <- function(x,y){(x-y) <= 0}
+  more2Vec <- function(x,y){(x-y) >= 0}
+  eqal2Vec <- function(x,y){(x-y) == 0}
+  ### ### ### ### ###
+  
+  if(discrete){
+    outLst <- outLst2 <- list()
+    ####discretized
+    for(j in 1:dim(rules)[1]){
+      
+      cnds <- cnd2[[j]]
+      cndsLen <- length(cnds)
+      vec3 <- list()
+      
+      for(i in 1:cndsLen){
+        vec3[[i]] <- as.numeric(as.data.frame(dt[,match(rl2[[j]], feats)])[,i] == cnd2[[j]][i])
+        #ifelse(length(vec4) == 0, vec4 <- vec3, vec4 <- vec3 & vec4)
+      }
+      outLst[[j]] <- rownames(dt)[which(rowSums(do.call(cbind, vec3)) == cndsLen)] ##LHS
+      dt2 <- dt[which(grepl(dec2[j], dt[,length(dt)])),]
+      outLst2[[j]] <- intersect(rownames(dt2), outLst[[j]]) ##RHS
+    } 
+  }else{
+    outLst <- outLst2 <- list()
+    
+    for(j in 1:dim(rules)[1]){
+      cnds <- cnd2[[j]]
+      cnds[cnds == "value>cut"] <- 1
+      cnds[cnds == "value<cut"] <- 1
+      cnds[cnds == "cut<value<cut"] <- 2
+      cnds <- as.numeric(cnds)
+      cndsLen <- length(cnds)
+      cndsCS <- cumsum(cnds)
+      vec4 <- c()
+      
+      for(i in 1:cndsLen){
+        
+        if(cnd2[[j]][i] == "value>cut"){
+          vec3 <- more2Vec(as.data.frame(dt[,match(rl2[[j]], feats)])[,i], as.numeric(cuts[j,][cndsCS[i]]))
+          ifelse(length(vec4) == 0, vec4 <- vec3, vec4 <- vec3 & vec4)
+        }
+        
+        if(cnd2[[j]][i] == "value<cut"){
+          vec3 <- less2Vec(as.data.frame(dt[,match(rl2[[j]], feats)])[,i], as.numeric(cuts[j,][cndsCS[i]]))
+          ifelse(length(vec4) == 0, vec4 <- vec3, vec4 <- vec3 & vec4)
+        }
+        
+        if(cnd2[[j]][i] == "cut<value<cut"){
+          vec1 <- less2Vec(as.data.frame(dt[,match(rl2[[j]], feats)])[,i], as.numeric(cuts[j,][cndsCS[i]]))
+          vec2 <- more2Vec(as.data.frame(dt[,match(rl2[[j]], feats)])[,i], as.numeric(cuts[j,][cndsCS[i]-1]))
+          vec3 <- vec1 == vec2
+          ifelse(length(vec4) == 0, vec4 <- vec3, vec4 <- vec3 & vec4)
+        }
+      }
+      
+      outLst[[j]] <- rownames(dt)[which(vec4)] ##LHS
+      dt2 <- dt[which(grepl(dec2[j], dt[,length(dt)])),]
+      outLst2[[j]] <- intersect(rownames(dt2),outLst[[j]])
+    }
+  }
+  
+  objectsPerRuleLHS <- unlist(lapply(outLst, function(x) paste(x, collapse = ",")))
+  objectsPerRuleRHS <- unlist(lapply(outLst2, function(x) paste(x, collapse = ",")))
+  newSupportLHS <- unlist(lapply(outLst, function(x) length(x)))
+  newSupportRHS <- unlist(lapply(outLst2, function(x) length(x)))
+  newAccuracy <- newSupportRHS/newSupportLHS
+  
+  ## p-value for rules calculation ##
+  PVAL <- c()
+  RISK_PVAL <- c()
+  CONF_INT <- c()
+  REL_RISK <- c()
+  
+  for(i in 1:length(newSupportLHS)){
+    # total support adjusted by accuracy
+    k <- round(newSupportRHS[i])-1 # P(X > k-1) <-> P(X >= k)
+    # num of samples for current decision - total white balls
+    R1 <- unname(table(dt[,length(dt)])[names(table(dt[,length(dt)]))==as.character(rules$decision[i])])
+    # num of samples for the rest samples - total black balls
+    N <- dim(dt)[1]
+    R2 <- N-R1
+    # the number of decisions/objects/patients
+    C1 <- newSupportLHS[i]   # exposed
+    C2 <- newSupportLHS[i]-k # non-exposed
+    
+    PVAL[i] <- phyper(q=k, m=R1, n=R2, k=C1, lower.tail = FALSE)  # calculate pvalue from phypergeometric
+    
+    # risk ratio
+    ge1 <- newSupportRHS[i]
+    #The number of disease occurence among exposed cohort.
+    ge2 <- newSupportLHS[i] - newSupportRHS[i] ## LHS > RHS
+    #The number of disease occurence among non-exposed cohort.
+    gt1 <- R1
+    #The number of individuals in exposed cohort group.
+    gt2 <- R2
+    #The number of individuals in non-exposed cohort group.
+    
+    invisible(capture.output(rr <- fmsb::riskratio(ge1, ge2, gt1, gt2)))
+    
+    ints <- rr$conf.int[1:2]
+    ints[is.na(ints)] <- -Inf
+    
+    CONF_INT[i] <- paste(as.character(round(ints, digits=3)), collapse =":") #rr 95% confidence intervals
+    RISK_PVAL[i] <- rr$p.value #rr p-value
+    REL_RISK[i] <- rr$estimate #risk ratio estimate
+  }
+  
+  if(pAdjust){
+    PVAL <- p.adjust(PVAL, method=pAdjustMethod)
+    RISK_PVAL <- p.adjust(RISK_PVAL, method=pAdjustMethod)
+  }
+  
+  decsCounts <- table(dt[,length(dt)])
+  
+  numClass <- rep(0,length(rules$decision))
+  for(i in 1:length(table(dt[,length(dt)]))){
+    numClass[which(rules$decision == names(table(dt[,length(dt)]))[i])] <- unname(table(dt[,length(dt)]))[i]
+  }
+  
+  percSuppLHS <- round(newSupportLHS/length(as.character(dt[,length(dt)])), digits=5)                         
+  percSuppRHS <- round(newSupportRHS/numClass, digits=5)                         
+  
+  if(discrete){
+    newDT <- data.frame(as.character(rules$features),as.character(rules$levels),as.character(rules$decision),newSupportLHS,newSupportRHS,newAccuracy,percSuppLHS,percSuppRHS,PVAL,  REL_RISK,RISK_PVAL, CONF_INT, objectsPerRuleLHS,objectsPerRuleRHS)
+    colnames(newDT) <- c("features","levels","decision","supportLHS","supportRHS","accuracyRHS","coverageLHS", "coverageRHS", "pValue", "riskRatio", "pValueRiskRatio", "confIntRiskRatio", "supportSetLHS", "supportSetRHS")
+    newDT2 <- newDT[order(newDT$pValue),]
+    rownames(newDT2) <- NULL
+  }else{
+    newDT <- data.frame(as.character(rules$features),as.character(rules$levels),as.character(rules$decision),newSupportLHS,newSupportRHS,newAccuracy,percSuppLHS,percSuppRHS,rules$cuts,cuts,PVAL,  REL_RISK,RISK_PVAL, CONF_INT, objectsPerRuleLHS,objectsPerRuleRHS)
+    colnames(newDT) <- c("features","levels","decision","supportLHS","supportRHS","accuracyRHS", "coverageLHS", "coverageRHS", "cuts",colnames(cuts), "pValue", "riskRatio", "pValueRiskRatio", "confIntRiskRatio", "supportSetLHS", "supportSetRHS")
+    newDT2 <- newDT[order(newDT$pValue),]
+    rownames(newDT2) <- NULL
+  }
+  i <- sapply(newDT2, is.factor)
+  newDT2[i] <- lapply(newDT2[i], as.character)
+  return(newDT2)
+}
